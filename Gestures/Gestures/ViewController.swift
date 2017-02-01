a91b968cba25307e71d38ae742377733f7733df5
yongrenwang <czie.alexmak@gmail.com>
2017-02-01 14:48:50 +0800

    Implemented IBActions that are called  on the ViewController in response to the gesture events

--- Gestures/Gestures/ViewController.swift
+++ Gestures/Gestures/ViewController.swift
@@ -18,6 +18,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        originalRect = imageView.frame
+        
+        var tempImageView: UIImageView
+        tempImageView = UIImageView(image: UIImage(named: "flower.png"))
+        tempImageView.frame = originalRect
+        view.addSubview(tempImageView)
+        self.imageView = tempImageView
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,14 +35,24 @@ class ViewController: UIViewController {
 
     
     @IBAction func foundTap(_ sender: UITapGestureRecognizer) {
+        outputLabel.text = "Tapped"
+        var location: CGPoint = sender.location(in: view)
+        print("You tapped at the coordinates: x - \(location.x), y - \(location.y)")
     }
    
     
     @IBAction func foundSwipe(_ sender: UISwipeGestureRecognizer) {
+        outputLabel.text = "Swipe"
     }
     
     
     @IBAction func foundPinch(_ sender: UIPinchGestureRecognizer) {
+        var feedback: String
+        var scale: CGFloat = sender.scale
+        imageView.transform = CGAffineTransform(rotationAngle: 0.0)
+        feedback = String(format: "Pinched, Scale: %1.2f, Velocity: %1.2f", Float(sender.scale),Float(sender.velocity))
+        outputLabel.text = feedback
+        imageView.frame = CGRect(origin: originalRect.origin, size: CGSize(width: scale*originalRect.size.width, height: scale*originalRect.size.height))
     }
     
     
